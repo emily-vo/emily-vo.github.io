f691594dd9f9c5467fcf293a049ed7cf359af8db
emily-vo <emvo@seas.upenn.edu>
2016-09-20 00:18:07 -0400

    Fix design slideshow to change pictures

--- assets/scripts/action.js
+++ assets/scripts/action.js
@@ -42,7 +42,15 @@ setInterval(function() {
     .end()
     .appendTo('#photo-slideshow');
 },  6000); 
-
+$("#design-slideshow > div:gt(0)").hide();
+setInterval(function() { 
+  $('#design-slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#design-slideshow');
+},  6000);
 $("#studies-slideshow > div:gt(0)").hide();
 setInterval(function() { 
   $('#studies-slideshow > div:first')
